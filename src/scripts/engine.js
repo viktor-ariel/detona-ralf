24056368ac9f799b0172aa7e3046d8d37bb087c5
Viktor Ariel <98894099+viktor-ariel@users.noreply.github.com>
2023-10-13 21:49:04 -0300

    Add files via upload

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,12 +6,31 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
         gameVelocity: 500,
+        hitPosition: 0,
+        result:0,
+        currentTime:30,
+    },
+    actions:{
+        timerId: setInterval(randomSquare, 500),
+        countDownTimerId: setInterval(countDown, 1000),
     },
 }
 
-
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeleft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0 ){
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+        alert("Seu Tempo Acabou!! Você fez um total de " + state.values.result)
+    }
+}
+function playSound(){
+    let audio = new Audio("./src/audio/hit.m4a")
+    audio.volume=0.2;
+    audio.play()
+}
 function randomSquare(){
     state.view.squares.forEach((square)=>{
         square.classList.remove("enemy");
@@ -19,18 +38,24 @@ function randomSquare(){
     let randaoNumber = Math.floor(Math.random()*9);
     let randaoSquare = state.view.squares[randaoNumber];
     randaoSquare.classList.add("enemy");
+    state.values.hitPosition = randaoSquare.id;
 }
-function moveEnemy(){
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
-
 function addListenerHitBox(){
-    state.view.squares.forEach((square) =>{});
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown",()=>{
+            if(square.id===state.values.hitPosition){
+                
+                state.values.result++;
+                state.view.score.textContent = state.values.result 
+                state.values.hitPosition= null;
+                playSound();
+            }
+            
+        });
+    });
 }
-
 //função para estartar algumas funções do jogo
 function initialise(){
-    moveEnemy();
+    addListenerHitBox();
 }
-
 initialise();
